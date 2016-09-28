af812cee1e7dd57905ccb85ab594f80a701edcfe
F74022214 <apple894894@gmail.com>
2016-09-28 21:08:29 +0800

    obj file loading and display complete
    Finally, OMG it's tiring

--- model.js
+++ model.js
@@ -1,16 +1,102 @@
+var loaded = 0;
 
-function Model(_vertices){
-	this.vertices = _vertices;
+function Model(){
+	this.vertices = [];
+	this.uvs = [];
+	this.normals = [];
 	this.rotate_matrix = mat4.create();
 	this.translate_matrix = mat4.create();
 	this.scale_matrix = mat4.create();
 	this.model_matrix = mat4.create();
+	mat4.identity(this.rotate_matrix);
+	mat4.identity(this.translate_matrix);
+	mat4.identity(this.scale_matrix);
+	mat4.identity(this.model_matrix);
 }
 
+Model.prototype.getModelMatrix = function(){
+	mat4.multiply(this.translate_matrix, this.rotate_matrix, this.model_matrix);
+	mat4.multiply(this.model_matrix, this.scale_matrix, this.model_matrix);
+	return this.model_matrix;
+}
 
-function readObjFile(file)
-{
-	$.get("cube.txt", function(data) {
+Model.prototype.sayVertices = function(){
+	alert(this.vertices);
+}
+
+loadObj = function(model, file) {
+
+	var out = document.getElementById('out');
+	var tmp_vertices=[], tmp_uvs=[], tmp_normals=[], tmps=[], f=[];
+	var indices=[];
+	$.get(file, function(data) {
         alert(data);
-    });
+    
+	    var substrings = data.replace( /\n/g, ' ').split(' ');
+	    /*for(var i=0; i<substrings.length; i++){
+	    	out.value+=substrings[i];
+	    	out.value+='\n';
+	    }*/
+
+	    for(var i=0; i<substrings.length; i++){
+	    	//alert(substrings[i]);
+	    	if(substrings[i] === "v"){
+				tmp_vertices.push(substrings[++i]);
+	    		tmp_vertices.push(substrings[++i]);
+	    		tmp_vertices.push(substrings[++i]);
+
+	    		//alert("v : "+tmps[0]+tmps[1]+tmps[2]);
+	    	}
+	    	if(substrings[i] === "vt"){
+	    		tmp_uvs.push(substrings[++i]);
+	    		tmp_uvs.push(substrings[++i]);
+	    	}
+	    	if(substrings[i] === "vn"){
+	    		tmp_normals.push(substrings[++i]);
+	    		tmp_normals.push(substrings[++i]);
+	    		tmp_normals.push(substrings[++i]);
+	    	}
+	    	if(substrings[i] === "f"){
+	    		f = [];
+	    		f[0] = substrings[i+1].split('/');
+	    		f.push(substrings[i+2].split('/'));
+	    		f.push(substrings[i+3].split('/'));
+
+	    		//alert(f[3]);
+
+	    		indices.push(f[0][0]);
+	    		indices.push(f[0][1]);
+	    		indices.push(f[0][2]);
+	    		indices.push(f[1][0]);
+	    		indices.push(f[1][1]);
+	    		indices.push(f[1][2]);
+	    		indices.push(f[2][0]);
+	    		indices.push(f[2][1]);
+	    		indices.push(f[2][2]);
+	    	}
+	    }
+	    model.vertices=[];
+	    model.uvs=[];
+	    model.normals=[];
+	    for (var i = 0; i < indices.length; i += 3) {
+			model.vertices.push(tmp_vertices[(indices[i] - 1) * 3]);
+			model.vertices.push(tmp_vertices[(indices[i] - 1) * 3 + 1]);
+			model.vertices.push(tmp_vertices[(indices[i] - 1) * 3 + 2]);
+			model.uvs.push(tmp_uvs[(indices[i + 1] - 1) * 2]);
+			model.uvs.push(tmp_uvs[(indices[i + 1] - 1) * 2+1]);
+			model.normals.push(tmp_normals[(indices[i + 2] - 1) * 3]);
+			model.normals.push(tmp_normals[(indices[i + 2] - 1) * 3 + 1]);
+			model.normals.push(tmp_normals[(indices[i + 2] - 1) * 3 + 2]);
+
+		}
+		loaded++;
+		//alert(model.vertices);
+		for(var i=0; i<model.vertices.length; i++){
+	    	out.value+=model.vertices[i];
+	    	out.value+='\n';
+	    }
+	});
+	//alert("QWQ");
+	//alert(model.vertices);
 }
+
--- webgl.js
+++ webgl.js
@@ -1,152 +1,177 @@
 var triangleVertexPositionBuffer;
 var squareVertexPositionBuffer;	
 
-	function webGLStart() {
-    var canvas = document.getElementById("lesson01-canvas");
-    initGL(canvas);
-    initShaders();
-    initBuffers();
+var cube = new Model();
+var inited = false;
 
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.enable(gl.DEPTH_TEST);
+function main(){
+	loadAllObj();
+	webGLStart();
+}
 
-    drawScene();
+function loadAllObj(){
+	loadObj(cube, "https://jackraken.github.io/wp2016/cube.obj");
+}
 
-    readObjFile("file:///C:/Users/LukeWu/git/AOCTDF/files/AgeOfCube/models/cube.obj");
-    tick();
-  }
+function webGLStart() {
+	if(loaded > 0){
+		var canvas = document.getElementById("lesson01-canvas");
 
-  function tick() {
-    //requestAnimFrame(tick);
-    drawScene();
-    //animate();
-  }
+	  initGL(canvas);
+	  initShaders();
+	  initBuffers();
 
-  var rTri = 0;
-  var rSquare = 0;
+	  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+	  gl.enable(gl.DEPTH_TEST);
 
-  function initBuffers() {
-    triangleVertexPositionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
-    var vertices = [
-         0.0,  1.0,  0.0,
-        -1.0, -1.0,  0.0,
-         1.0, -1.0,  0.0
-    ];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    triangleVertexPositionBuffer.itemSize = 3;
-    triangleVertexPositionBuffer.numItems = 3;
+	  drawScene();
 
-    squareVertexPositionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
-    vertices = [
-         1.0,  1.0,  0.0,
-        -1.0,  1.0,  0.0,
-         1.0, -1.0,  0.0,
-        -1.0, -1.0,  0.0
-    ];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    squareVertexPositionBuffer.itemSize = 3;
-    squareVertexPositionBuffer.numItems = 4;
-  }
-
-  function degToRad(degrees) {
-      return degrees * Math.PI / 180;
-  }
-
-  function drawScene() {
-    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
-    mat4.identity(mvMatrix);
-    mat4.translate(mvMatrix, [-1.5, 0.0, -7.0]);
-
-		mat4.rotate(mvMatrix, degToRad(rTri), [0, 1, 0]);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
-    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, triangleVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
-    setMatrixUniforms();
-    gl.drawArrays(gl.TRIANGLES, 0, triangleVertexPositionBuffer.numItems);
-    mat4.translate(mvMatrix, [3.0, 0.0, 0.0]);
-    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
-    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, squareVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
-    setMatrixUniforms();
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, squareVertexPositionBuffer.numItems);
+	  tick();
 	}
+	else{
+		setTimeout(webGLStart, 500);
+	}
+  
+}
 
-	var gl;
-  function initGL(canvas) {
-    try {
-      gl = canvas.getContext("experimental-webgl");
-      gl.viewportWidth = canvas.width;
-      gl.viewportHeight = canvas.height;
-    } catch(e) {
+function tick() {
+  //requestAnimFrame(tick);
+  //drawScene();
+  //animate();
+}
+
+var rTri = 0;
+var rSquare = 0;
+
+function initBuffers() {
+
+  triangleVertexPositionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
+  var vertices = [
+       0.0,  1.0,  0.0,
+      -1.0, -1.0,  0.0,
+       1.0, -1.0,  0.0
+  ];
+  //cube.sayVertices();
+  //alert("123");
+  //alert(cube.vertices);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cube.vertices), gl.STATIC_DRAW);
+  triangleVertexPositionBuffer.itemSize = 3;
+  triangleVertexPositionBuffer.numItems = cube.vertices.length/3;
+
+  squareVertexPositionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
+  vertices = [
+       1.0,  1.0,  0.0,
+      -1.0,  1.0,  0.0,
+       1.0, -1.0,  0.0,
+      -1.0, -1.0,  0.0
+  ];
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  squareVertexPositionBuffer.itemSize = 3;
+  squareVertexPositionBuffer.numItems = 4;
+}
+
+function degToRad(degrees) {
+    return degrees * Math.PI / 180;
+}
+
+function drawScene() {
+  gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
+  mat4.identity(vMatrix);
+  mat4.translate(vMatrix, [-1.5, 0.0, -7.0]);
+
+	mat4.rotate(vMatrix, degToRad(rTri), [0, 1, 0]);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
+  gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, triangleVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+  setMatrixUniforms();
+  gl.drawArrays(gl.TRIANGLES, 0, triangleVertexPositionBuffer.numItems);
+  
+  mat4.translate(vMatrix, [3.0, 0.0, 0.0]);
+  gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
+  gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, squareVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+  setMatrixUniforms();
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, squareVertexPositionBuffer.numItems);
+}
+
+var gl;
+function initGL(canvas) {
+  try {
+    gl = canvas.getContext("experimental-webgl");
+    gl.viewportWidth = canvas.width;
+    gl.viewportHeight = canvas.height;
+  } catch(e) {
+  }
+  if (!gl) {
+    alert("Could not initialise WebGL, sorry :-( ");
+  }
+}
+
+var vMatrix = mat4.create();
+var pMatrix = mat4.create();
+
+var shaderProgram;
+function initShaders() {
+  var fragmentShader = getShader(gl, "shader-fs");
+  var vertexShader = getShader(gl, "shader-vs");
+
+  shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    alert("Could not initialise shaders");
+  }
+
+  gl.useProgram(shaderProgram);
+  shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+  gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+
+  shaderProgram.mMatrixUniform = gl.getUniformLocation(shaderProgram, "M");
+  shaderProgram.vMatrixUniform = gl.getUniformLocation(shaderProgram, "V");
+  shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "P");
+}
+
+function getShader(gl, id) {
+    var shaderScript = document.getElementById(id);
+    if (!shaderScript) {
+        return null;
     }
-    if (!gl) {
-      alert("Could not initialise WebGL, sorry :-( ");
-    }
-  }
 
-  var mvMatrix = mat4.create();
-  var pMatrix = mat4.create();
-
-  var shaderProgram;
-  function initShaders() {
-    var fragmentShader = getShader(gl, "shader-fs");
-    var vertexShader = getShader(gl, "shader-vs");
-
-    shaderProgram = gl.createProgram();
-    gl.attachShader(shaderProgram, vertexShader);
-    gl.attachShader(shaderProgram, fragmentShader);
-    gl.linkProgram(shaderProgram);
-
-    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-      alert("Could not initialise shaders");
+    var str = "";
+    var k = shaderScript.firstChild;
+    while (k) {
+        if (k.nodeType == 3)
+            str += k.textContent;
+        k = k.nextSibling;
     }
 
-    gl.useProgram(shaderProgram);
-    shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
-    gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+    var shader;
+    if (shaderScript.type == "x-shader/x-fragment") {
+        shader = gl.createShader(gl.FRAGMENT_SHADER);
+    } else if (shaderScript.type == "x-shader/x-vertex") {
+        shader = gl.createShader(gl.VERTEX_SHADER);
+    } else {
+        return null;
+    }
 
-    shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
-    shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
-  }
+    gl.shaderSource(shader, str);
+    gl.compileShader(shader);
 
-  function getShader(gl, id) {
-      var shaderScript = document.getElementById(id);
-      if (!shaderScript) {
-          return null;
-      }
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert(gl.getShaderInfoLog(shader));
+        return null;
+    }
 
-      var str = "";
-      var k = shaderScript.firstChild;
-      while (k) {
-          if (k.nodeType == 3)
-              str += k.textContent;
-          k = k.nextSibling;
-      }
+    return shader;
+}
 
-      var shader;
-      if (shaderScript.type == "x-shader/x-fragment") {
-          shader = gl.createShader(gl.FRAGMENT_SHADER);
-      } else if (shaderScript.type == "x-shader/x-vertex") {
-          shader = gl.createShader(gl.VERTEX_SHADER);
-      } else {
-          return null;
-      }
-
-      gl.shaderSource(shader, str);
-      gl.compileShader(shader);
-
-      if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-          alert(gl.getShaderInfoLog(shader));
-          return null;
-      }
-
-      return shader;
-  }
-
-  function setMatrixUniforms() {
-    gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
-    gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
-  }
+function setMatrixUniforms() {
+  gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
+  gl.uniformMatrix4fv(shaderProgram.vMatrixUniform, false, vMatrix);
+  gl.uniformMatrix4fv(shaderProgram.mMatrixUniform, false, cube.getModelMatrix());
+}

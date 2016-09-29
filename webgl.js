cca0d81054a87de2c3b8a24a3b43d6726b0a2d12
F74022214 <apple894894@gmail.com>
2016-09-29 10:22:01 +0800

    debugging

--- webgl.js
+++ webgl.js
@@ -194,15 +194,16 @@ function initTexture() {
   texture_id.image.onload = function() {
     handleLoadedTexture(texture_id)
   }
-
   texture_id.image.src = "test.bmp";
+  
 }
 
 function handleLoadedTexture(texture) {
   gl.bindTexture(gl.TEXTURE_2D, texture);
   gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, texture.image);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
   gl.bindTexture(gl.TEXTURE_2D, null);
 }
+
